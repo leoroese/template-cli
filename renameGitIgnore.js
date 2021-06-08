777e5d2102db509eb25340ec3127ab38e0b69d9f
Leonardo Roese <leo.roese@hotmail.com>
2021-06-08 17:48:50 -0600

    refactor: Cleanup

--- renameGitIgnore.js
+++
@@ -1,9 +0,0 @@
-if (stats.isFile()) {
-  const contents = fs.readFileSync(origFilePath, 'utf8');
-
-  // Rename
-  if (file === '.npmignore') file = '.gitignore';
-
-  const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
-  fs.writeFileSync(writePath, contents, 'utf8');
-}
